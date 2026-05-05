f8943299070a91d3c584c6cb29e9f72c87346ec8
deo-anggoro <bancet548@gmail.com>
2026-05-05 15:50:51 +0700

    deploy in railway

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tu1+(4upg(8c_+k0_=613
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1,gamefacepuzzle-py-production.up.railway.app'
+).split(',')
 CSRF_TRUSTED_ORIGINS = [f'https://{h.strip()}' for h in ALLOWED_HOSTS if h.strip()]
 
 # Render.com sets RENDER env var
